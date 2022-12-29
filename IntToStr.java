ce497ff73734ef56322abbf7886b1d84bcf5cc31
Ananya-Jha1208 <ananyamaitrayijha@gmail.com>
2022-12-29 12:40:51 +0530

    hackerrank

---
+++ IntToStr.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class IntToStr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = Integer.toString(n);
+        System.out.println("Good job");
+    }
+}
