ce8d0e409077a3403a6bbfc065996c215b228b9d
Ananya-Jha1208 <ananyamaitrayijha@gmail.com>
2023-01-02 13:15:31 +0530

    hackerrank

---
+++ List.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class List {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> L = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            L.add(sc.nextInt());
+        }
+        int Q = sc.nextInt();
+        for (int j = 0; j < Q; j++) {
+            String s = sc.next();
+            if (s.equals("Insert")) {
+                L.add(sc.nextInt(), sc.nextInt());
+            } else if (s.equals("Delete")) {
+                L.remove(sc.nextInt());
+            }
+        }
+        sc.close();
+        L.forEach(i -> System.out.print(i + " "));
+    }
+}
